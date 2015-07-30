ff447be8be857ca98f19bc96adbb5e6defd2249e
vicmatmar <victormartin@hotmail.com>
2015-07-30 17:32:23 -0500

    DIO settings updates

--- powercal/FormDigitalPortTest.Designer.cs
+++ powercal/FormDigitalPortTest.Designer.cs
@@ -33,18 +33,19 @@
             this.physicalChannelLabel = new System.Windows.Forms.Label();
             this.writeButton = new System.Windows.Forms.Button();
             this.dataLabel = new System.Windows.Forms.Label();
-            this.dataToWriteNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDowndataToWrite = new System.Windows.Forms.NumericUpDown();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
             this.NumericUpDownOutput = new System.Windows.Forms.NumericUpDown();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelReset = new System.Windows.Forms.Label();
             this.NumericUpDownReset = new System.Windows.Forms.NumericUpDown();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labelLoad = new System.Windows.Forms.Label();
             this.NumericUpDownLoad = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labelACPower = new System.Windows.Forms.Label();
             this.NumericUpDownACPower = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
             this.channelParametersGroupBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataToWriteNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDowndataToWrite)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownOutput)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownReset)).BeginInit();
@@ -100,19 +101,19 @@
             this.dataLabel.TabIndex = 6;
             this.dataLabel.Text = "Data to Write:";
             // 
-            // dataToWriteNumericUpDown
+            // NumericUpDowndataToWrite
             // 
-            this.dataToWriteNumericUpDown.Hexadecimal = true;
-            this.dataToWriteNumericUpDown.Location = new System.Drawing.Point(26, 118);
-            this.dataToWriteNumericUpDown.Maximum = new decimal(new int[] {
+            this.NumericUpDowndataToWrite.Hexadecimal = true;
+            this.NumericUpDowndataToWrite.Location = new System.Drawing.Point(47, 120);
+            this.NumericUpDowndataToWrite.Maximum = new decimal(new int[] {
             255,
             0,
             0,
             0});
-            this.dataToWriteNumericUpDown.Name = "dataToWriteNumericUpDown";
-            this.dataToWriteNumericUpDown.Size = new System.Drawing.Size(64, 20);
-            this.dataToWriteNumericUpDown.TabIndex = 7;
-            this.dataToWriteNumericUpDown.Value = new decimal(new int[] {
+            this.NumericUpDowndataToWrite.Name = "NumericUpDowndataToWrite";
+            this.NumericUpDowndataToWrite.Size = new System.Drawing.Size(54, 20);
+            this.NumericUpDowndataToWrite.TabIndex = 7;
+            this.NumericUpDowndataToWrite.Value = new decimal(new int[] {
             255,
             0,
             0,
@@ -120,13 +121,13 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.labelOutput);
             this.groupBox2.Controls.Add(this.NumericUpDownOutput);
-            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.labelReset);
             this.groupBox2.Controls.Add(this.NumericUpDownReset);
-            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.labelLoad);
             this.groupBox2.Controls.Add(this.NumericUpDownLoad);
-            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.labelACPower);
             this.groupBox2.Controls.Add(this.NumericUpDownACPower);
             this.groupBox2.Location = new System.Drawing.Point(26, 171);
             this.groupBox2.Name = "groupBox2";
@@ -135,18 +136,18 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "DIO Line Values";
             // 
-            // label6
+            // labelOutput
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 89);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 13);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "Output";
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(7, 89);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(39, 13);
+            this.labelOutput.TabIndex = 7;
+            this.labelOutput.Text = "Output";
             // 
             // NumericUpDownOutput
             // 
-            this.NumericUpDownOutput.Location = new System.Drawing.Point(67, 88);
+            this.NumericUpDownOutput.Location = new System.Drawing.Point(96, 87);
             this.NumericUpDownOutput.Maximum = new decimal(new int[] {
             1,
             0,
@@ -157,18 +158,18 @@
             this.NumericUpDownOutput.TabIndex = 6;
             this.NumericUpDownOutput.ValueChanged += new System.EventHandler(this.NumericUpDownOutput_ValueChanged);
             // 
-            // label5
+            // labelReset
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 65);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Reset";
+            this.labelReset.AutoSize = true;
+            this.labelReset.Location = new System.Drawing.Point(7, 65);
+            this.labelReset.Name = "labelReset";
+            this.labelReset.Size = new System.Drawing.Size(35, 13);
+            this.labelReset.TabIndex = 5;
+            this.labelReset.Text = "Reset";
             // 
             // NumericUpDownReset
             // 
-            this.NumericUpDownReset.Location = new System.Drawing.Point(67, 64);
+            this.NumericUpDownReset.Location = new System.Drawing.Point(96, 63);
             this.NumericUpDownReset.Maximum = new decimal(new int[] {
             1,
             0,
@@ -179,18 +180,18 @@
             this.NumericUpDownReset.TabIndex = 4;
             this.NumericUpDownReset.ValueChanged += new System.EventHandler(this.NumericUpDownReset_ValueChanged);
             // 
-            // label4
+            // labelLoad
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 43);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Load";
+            this.labelLoad.AutoSize = true;
+            this.labelLoad.Location = new System.Drawing.Point(7, 43);
+            this.labelLoad.Name = "labelLoad";
+            this.labelLoad.Size = new System.Drawing.Size(31, 13);
+            this.labelLoad.TabIndex = 3;
+            this.labelLoad.Text = "Load";
             // 
             // NumericUpDownLoad
             // 
-            this.NumericUpDownLoad.Location = new System.Drawing.Point(67, 42);
+            this.NumericUpDownLoad.Location = new System.Drawing.Point(96, 41);
             this.NumericUpDownLoad.Maximum = new decimal(new int[] {
             1,
             0,
@@ -201,18 +202,18 @@
             this.NumericUpDownLoad.TabIndex = 2;
             this.NumericUpDownLoad.ValueChanged += new System.EventHandler(this.NumericUpDownLoad_ValueChanged);
             // 
-            // label3
+            // labelACPower
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "AC Power";
+            this.labelACPower.AutoSize = true;
+            this.labelACPower.Location = new System.Drawing.Point(7, 20);
+            this.labelACPower.Name = "labelACPower";
+            this.labelACPower.Size = new System.Drawing.Size(54, 13);
+            this.labelACPower.TabIndex = 1;
+            this.labelACPower.Text = "AC Power";
             // 
             // NumericUpDownACPower
             // 
-            this.NumericUpDownACPower.Location = new System.Drawing.Point(67, 19);
+            this.NumericUpDownACPower.Location = new System.Drawing.Point(96, 18);
             this.NumericUpDownACPower.Maximum = new decimal(new int[] {
             1,
             0,
@@ -223,23 +224,33 @@
             this.NumericUpDownACPower.TabIndex = 0;
             this.NumericUpDownACPower.ValueChanged += new System.EventHandler(this.NumericUpDownACPower_ValueChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "0x";
+            // 
             // FormDigitalPortTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(334, 405);
+            this.ClientSize = new System.Drawing.Size(235, 348);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.channelParametersGroupBox);
             this.Controls.Add(this.writeButton);
             this.Controls.Add(this.dataLabel);
-            this.Controls.Add(this.dataToWriteNumericUpDown);
+            this.Controls.Add(this.NumericUpDowndataToWrite);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(200, 200);
             this.Name = "FormDigitalPortTest";
             this.Text = "Write to Digital Port";
             this.channelParametersGroupBox.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataToWriteNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDowndataToWrite)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownOutput)).EndInit();
@@ -247,6 +258,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownLoad)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownACPower)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -257,15 +269,16 @@
         private System.Windows.Forms.Label physicalChannelLabel;
         private System.Windows.Forms.Button writeButton;
         private System.Windows.Forms.Label dataLabel;
-        private System.Windows.Forms.NumericUpDown dataToWriteNumericUpDown;
+        private System.Windows.Forms.NumericUpDown NumericUpDowndataToWrite;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelOutput;
         public System.Windows.Forms.NumericUpDown NumericUpDownOutput;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelReset;
         public System.Windows.Forms.NumericUpDown NumericUpDownReset;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelLoad;
         public System.Windows.Forms.NumericUpDown NumericUpDownLoad;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelACPower;
         public System.Windows.Forms.NumericUpDown NumericUpDownACPower;
+        private System.Windows.Forms.Label label1;
     }
 }
--- powercal/FormDigitalPortTest.cs
+++ powercal/FormDigitalPortTest.cs
@@ -20,6 +20,25 @@ namespace powercal
 
             initphysicalChannelComboBox();
 
+            // Add line numbers to labels
+            int linenum = Properties.Settings.Default.DIO_ACPower_LineNum;
+            _relayCtrl.AC_Power_LineNum = linenum;
+            labelACPower.Text += string.Format("({0})", linenum);
+
+            linenum = Properties.Settings.Default.DIO_Load_LinNum;
+            _relayCtrl.Load_LineNum = linenum;
+            labelLoad.Text += string.Format("({0})", linenum);
+
+            linenum = Properties.Settings.Default.DIO_Reset_LineNum;
+            _relayCtrl.Reset_LineNum = linenum;
+            labelReset.Text += string.Format("({0})", linenum);
+
+            linenum = Properties.Settings.Default.DIO_Output_LineNum;
+            _relayCtrl.Output_LineNum = linenum;
+            labelOutput.Text += string.Format("({0})", linenum);
+
+            NumericUpDowndataToWrite.Value = rearPort();
+
             refreshNumericUpDownValue();
         }
 
@@ -70,7 +89,7 @@ namespace powercal
                     //  Write digital port data. WriteDigitalSingChanSingSampPort writes a single sample
                     //  of digital data on demand, so no timeout is necessary.
                     DigitalSingleChannelWriter writer = new DigitalSingleChannelWriter(digitalWriteTask.Stream);
-                    writer.WriteSingleSamplePort(true, (UInt32)dataToWriteNumericUpDown.Value);
+                    writer.WriteSingleSamplePort(true, (UInt32)NumericUpDowndataToWrite.Value);
                 }
 
                 refreshNumericUpDownValue();
@@ -86,29 +105,61 @@ namespace powercal
             }
         }
 
+        private byte rearPort()
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            byte data = 0;
+            try
+            {
+                using (Task digitalReadTask = new Task())
+                {
+
+                    //  Create an Digital Output channel and name it.
+                    digitalReadTask.DOChannels.CreateChannel(physicalChannelComboBox.Text, "port0", ChannelLineGrouping.OneChannelForAllLines);
+
+                    //  Read digital port data. 
+                    DigitalSingleChannelReader reader = new DigitalSingleChannelReader(digitalReadTask.Stream);
+                    data = reader.ReadSingleSamplePortByte();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+
+            return data;
+        }
+
         private void NumericUpDownACPower_ValueChanged(object sender, EventArgs e)
         {
             NumericUpDown num = (NumericUpDown)sender;
             _relayCtrl.AC_Power = Convert.ToBoolean(num.Value);
+            NumericUpDowndataToWrite.Value = rearPort();
         }
 
         private void NumericUpDownLoad_ValueChanged(object sender, EventArgs e)
         {
             NumericUpDown num = (NumericUpDown)sender;
             _relayCtrl.Load = Convert.ToBoolean(num.Value);
+            NumericUpDowndataToWrite.Value = rearPort();
         }
 
         private void NumericUpDownReset_ValueChanged(object sender, EventArgs e)
         {
             NumericUpDown num = (NumericUpDown)sender;
             _relayCtrl.Reset = Convert.ToBoolean(num.Value);
+            NumericUpDowndataToWrite.Value = rearPort();
         }
 
         private void NumericUpDownOutput_ValueChanged(object sender, EventArgs e)
         {
             NumericUpDown num = (NumericUpDown)sender;
             _relayCtrl.Output = Convert.ToBoolean(num.Value);
-
+            NumericUpDowndataToWrite.Value = rearPort();
         }
     }
 }
