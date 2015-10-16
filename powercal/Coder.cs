42e9800e4286d88c5f2be4d75a11522af45f20fc
vicmatmar <victormartin@hotmail.com>
2015-10-16 09:02:52 -0500

    Coder timing tweaks

--- powercal/Coder.cs
+++ powercal/Coder.cs
@@ -32,6 +32,78 @@ namespace powercal
             _timeout = timeout;
         }
 
+        public void Code(CancellationToken cancel)
+        {
+            if (cancel.IsCancellationRequested)
+                return;
+
+            IntPtr hwnd = activateMainWnd();
+            moveToStatus();
+            
+            //AutoItX.Send("{SPACE}");
+            click_Start();
+            moveToStatus();
+            Thread.Sleep(2000);
+
+            int pixel_color = 0;
+            string msg;
+            int fail_count = 0;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            while (true)
+            {
+                if (cancel.IsCancellationRequested)
+                    break;
+
+                pixel_color = getStatusColor();
+
+                if (pixel_color == _cred)
+                {
+
+                    fail_count++;
+
+                    if (fail_count > 3)
+                    {
+                        click_Start();
+                        moveToStatus();
+                        Thread.Sleep(2000);
+
+                        msg = string.Format("Red pixel at location X={0}, Y={1} after {2:F2} s",
+                            _point_State_color.X, _point_State_color.Y, watch.Elapsed.TotalSeconds);
+                        throw new Exception(msg);
+                    }
+                    else
+                    {
+                        click_Retry();
+                        moveToStatus();
+                        Thread.Sleep(2000);
+                    }
+                }
+                else if (pixel_color == _cgreen)
+                {
+                    break;
+                }
+
+
+                Thread.Sleep(1000);
+                if (watch.Elapsed > _timeout)
+                {
+                    click_Start(); ;
+                    moveToStatus();
+
+                    msg = string.Format("Timeout after {0:hh\\:mm\\:ss}. Unable to detect green or read pixel at location X={1}, Y={2}",
+                        watch.Elapsed, _point_State_color.X, _point_State_color.Y);
+                    throw new Exception(msg);
+                }
+
+                if (AutoItX.WinActive(hwnd) == 0)
+                {
+                    hwnd = activateMainWnd();
+                    moveToStatus();
+                }
+            }
+        }
+
         IntPtr getWin(string win_desc)
         {
             string msg;
@@ -43,7 +115,7 @@ namespace powercal
             }
 
             IntPtr hwnd = AutoItX.WinGetHandle(win_desc);
-            if (AutoItX.WinExists(win_desc) != 1)
+            if (hwnd == IntPtr.Zero)
             {
                 msg = string.Format("Unable to get handle for window \"{0}\"", win_desc);
                 throw new Exception(msg);
@@ -84,76 +156,15 @@ namespace powercal
             return hwnd;
         }
 
-        public void Code(CancellationToken cancel)
+
+        void moveToStatus()
         {
-            if (cancel.IsCancellationRequested)
-                return;
+            AutoItX.MouseMove(_point_State_color.X, _point_State_color.Y, 20);
+        }
 
-            IntPtr hwnd = activateMainWnd();
-            AutoItX.MouseMove(_point_State_color.X, _point_State_color.Y, 0);
-            
-            //AutoItX.Send("{SPACE}");
-            click_Start();
-            Thread.Sleep(2000);
-
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            int pixel_color = 0;
-            string msg;
-            int fail_count = 0;
-            while (true)
-            {
-                if (cancel.IsCancellationRequested)
-                    break;
-
-                pixel_color = AutoItX.PixelGetColor(_point_State_color.X, _point_State_color.Y);
-                AutoItX.MouseMove(_point_State_color.X, _point_State_color.Y, 20);
-
-                if (pixel_color == _cred)
-                {
-
-                    fail_count++;
-
-                    if (fail_count > 3)
-                    {
-                        click_Start();
-                        Thread.Sleep(1000);
-
-                        msg = string.Format("Red pixel at location X={0}, Y={1} after {2:F2} s",
-                            _point_State_color.X, _point_State_color.Y, watch.Elapsed.TotalSeconds);
-                        throw new Exception(msg);
-                    }
-                    else
-                    {
-                        click_Retry();
-                        Thread.Sleep(1000);
-                    }
-                }
-                else if (pixel_color == _cgreen)
-                {
-                    break;
-                }
-
-
-                Thread.Sleep(500);
-                if (watch.Elapsed > _timeout)
-                {
-                    click_Start(); ;
-                    Thread.Sleep(1000);
-
-                    AutoItX.MouseMove(_point_State_color.X, _point_State_color.Y, 10);
-
-                    msg = string.Format("Timeout after {0:hh\\:mm\\:ss}. Unable to detect green or read pixel at location X={1}, Y={2}",
-                        watch.Elapsed, _point_State_color.X, _point_State_color.Y);
-                    throw new Exception(msg);
-                }
-
-                if (AutoItX.WinActive(hwnd) == 0)
-                {
-                    AutoItX.WinActivate(hwnd);
-                    AutoItX.MouseMove(_point_State_color.X, _point_State_color.Y, 0);
-                }
-            }
+        int getStatusColor()
+        {
+            return AutoItX.PixelGetColor(_point_State_color.X, _point_State_color.Y);
         }
 
         private void click_Start()
@@ -216,8 +227,8 @@ namespace powercal
         {
             Rectangle rec_grid = getGridPos();
 
-            int x = rec_grid.X + 215;
-            int y = rec_grid.Y + 30;
+            int x = rec_grid.X + 290;
+            int y = rec_grid.Y + 32;
 
             return new Point(x, y);
         }
